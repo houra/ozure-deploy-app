8a94d6df8a464fdc9fb9447c3aff34a14c6e0292
ohiro <hiroto.oura@thebitcellar.com>
2015-10-01 10:41:55 +0900

    git の url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'sample-app-html'
 
 set :deploy_to, "/deploy/#{fetch :application}"
 
-set :repo_url, 'git@github.com:ohiro/sample-app-html.git'
+set :repo_url, 'git@github.com:ohiro/ozure-deploy-app.git'
 
 set :keep_releases, 5
 
